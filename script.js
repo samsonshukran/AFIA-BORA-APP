bcb496a0c522bbe7a373e680d039f5baed878877
samsonshukran <samsonkalume44@gmail.com>
2025-09-13 10:41:33 +0300

    Initial commit - Afia Bora  App

---
+++ script.js
@@ -0,0 +1,41 @@
+
+    let currentSlide = 0;
+    const slides = document.querySelectorAll(".highlight-slide");
+    const dots = document.querySelectorAll(".dot");
+
+    function showSlide(index) {
+      slides[currentSlide].classList.remove("active");
+      dots[currentSlide].classList.remove("active");
+      currentSlide = index;
+      slides[currentSlide].classList.add("active");
+      dots[currentSlide].classList.add("active");
+    }
+
+    // Auto-rotate every 5 seconds
+    setInterval(() => {
+      let nextSlide = (currentSlide + 1) % slides.length;
+      showSlide(nextSlide);
+    }, 5000);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
